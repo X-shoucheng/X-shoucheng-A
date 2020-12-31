df86a0138dab213ba198057fb838113114cf083b
X-shoucheng <xiashoucheng@126.com>
2020-12-31 11:08:50 +0800

    第一次提交给GitHub

---
+++ Project1/Project1/test.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	///*char arr1[] = "abc";
+//	//char arr2[] = {'a','b','c','\0'};
+//	//printf("%d\n", strlen(arr1));
+//	//printf("%d\n", strlen(arr2));*/
+//	printf("%d\n",strlen("c:\test\32\test.c"));
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	printf("学习编程\n");
+//
+//	while(line<20000)
+//	{
+//		printf("敲代码:%d\n",line);
+//		line++;
+//	}
+//	printf("成功\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int i = 0;
+//	while(i<10)
+//	{
+//		printf("%d ", arr[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	while (a <= 100)
+//	{	
+//		if (a%2 == 1)
+//		{
+//			printf("%d\n",a);
+//		}
+//			
+//		a++;
+//	}
+//
+//		return 0;
+//}
+int main()
+{
+    int i    =1;
+	while(i <= 10 )
+	{
+	    printf("%d\n",i);
+	    i++;
+	}
+	return 0;
+}
+
