6f183cf410f612e54f3d6bb8f8a6204c55bf3467
X-shoucheng <xiashoucheng@126.com>
2021-01-27 15:04:46 +0800

    加了注释

--- 数组/三子棋游戏代码/game.c
+++ 数组/三子棋游戏代码/game.c
@@ -1,6 +1,5 @@
-//主程序调用的函数
+#include "game.h"；//引用头文件
 
-#include "game.h"；
 void InitBoard(char board[ROW][COL], int row, int col)//初始化棋盘
 {
     int i = 0;
@@ -19,18 +18,18 @@ void DisplayBoard(char board[ROW][COL], int row, int col)//打印棋盘
     for (i = 0; i < row; i++)
     {
         int j = 0;
-        for (j = 0; j < col; j++)//打印一行
+        for (j = 0; j < col; j++)//打印一行，棋盘格加竖线
         {
             printf(" %c ", board[i][j]);//打印空格+数组符号+空格
             if (j < col - 1)
             {
-                printf("|");//在最后一行前打印竖线
+                printf("|");//在上面一串之后打印竖线，但最后一列不用打印
             }
         }
         printf("\n");
         if (i < row - 1)
         {
-            for (j = 0; j < col; j++)
+            for (j = 0; j < col; j++)//再打印一行，每行棋盘格直接的分隔线
             {
                 printf("---");
                 if (j < col - 1)
@@ -54,11 +53,11 @@ void PlayerMove(char board[ROW][COL], int row, int col)
         scanf("%d,%d", &x, &y);
         printf("\n");
         //判断坐标合法性
-        if (x >= 1 && x <= row && y >= 1 && y <= col)
+        if (x >= 1 && x <= row && y >= 1 && y <= col) //默认棋盘坐标以（1,1）开始
         {
             if (board[x - 1][y - 1] == ' ')
             {
-                board[x - 1][y - 1] = '*';
+                board[x - 1][y - 1] = '*';//在玩家输入的坐标里放上*
                 break;
             }
             else
@@ -81,11 +80,11 @@ void ComputerMove(char board[ROW][COL], int row, int col)
 
     while (1)
     {
-        x = rand() % row;
+        x = rand() % row;//产生随机数让电脑走，模的值一定在0~row-1之间
         y = rand() % col;
         if (board[x][y] == ' ')
         {
-            board[x][y] = 'o';
+            board[x][y] = 'o';//在电脑产生的坐标上放o
             break;
         }
     }
@@ -111,9 +110,9 @@ char IsWin(char board[ROW][COL], int row, int col)
     int i = 0;
     for (i = 0; i < row; i++) //横三行
     {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')//仅仅适用于三行三列
         {
-            return board[i][1];
+            return board[i][1];//不用判断是谁下的，只要返回棋盘的值就行
         }
     }
     for (i = 0; i < col; i++) //竖三列
